f62c7cee035092996fe230a50b57120c0da25454
nuenuey <162126911+nuenuey@users.noreply.github.com>
2024-10-26 18:02:31 +0900

    8주차 최종과제코드

---
+++ hw09/single_pages/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+
+app_name = 'single_pages'
+
+urlpatterns = [
+    path('', views.landing_page, name='index'),
+    path('dodo/', views.dodo_page, name='dodo'),
+    path('flower/', views.flower_page, name='flower'),
+    path('king/', views.king_page, name='king'),
+    path('onedanggo/', views.onedanggo_page, name='onedanggo'),
+    path('posts/', views.posts_page, name='posts'),
+    path('soba/', views.soba_page, name='soba'),
+    path('soonday/', views.soonday_page, name='soonday'),
+]
