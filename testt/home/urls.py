0e57dca74619bccf1c6b1f8f3b75c212ffc1bce3
nuenuey <162126911+nuenuey@users.noreply.github.com>
2024-12-21 11:40:58 +0000

    돼지박사 예약페이지까지

--- testt/home/urls.py
+++ testt/home/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
-
+from .views import PostList, PostDetail, reserve
 app_name = "home"
 
 urlpatterns = [
     path('', views.PostList.as_view()),
     path('<int:pk>/', views.PostDetail.as_view()),
+    path('reserve/', views.reserve, name='reserve'),
+    path('reserve/process/', views.reserve_process, name='reserve_process'),
 ]
--- testt/home/views.py
+++ testt/home/views.py
@@ -1,6 +1,18 @@
 from django.views.generic import ListView, DetailView
 from .models import Post
+from django.shortcuts import render
+from django.http import HttpResponse
 
+def reserve(request):
+    return render(request, 'home/reserve.html')
+
+def reserve_process(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        date = request.POST.get('date')
+        # 예약 처리를 수행하고, 성공 메시지 또는 에러 메시지를 반환
+        return HttpResponse(f'예약 완료! 이름: {name}, 날짜: {date}')
+    return HttpResponse('잘못된 요청입니다.')
 # Create your views here.
 
 class PostList(ListView):
